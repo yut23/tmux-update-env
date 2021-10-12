57057b5d3bb2b3a48382faf23a084d2b1d6c1998
yut23 <yut23@gvljohnsons.com>
2021-10-11 23:05:42 -0400

    Get env file path at initialization

--- tmux-update-env.plugin.zsh
+++ tmux-update-env.plugin.zsh
@@ -1,13 +1,10 @@
 if [[ -n $TMUX ]]; then
   0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
   0="${${(M)0:#/*}:-$PWD/$0}"
+  _tmux_update_env_path="$(tmux display-message -p "$("${0:h}/scripts/get_env_path.sh")")"
 
   _tmux_update_env_last_change=$(date +%s)
   function _update_tmux_env() {
-    if ! (( $+_tmux_update_env_path )); then
-      # get path to environment file
-      _tmux_update_env_path="$(tmux display-message -p "$("${0:h}/scripts/get_env_path.sh")")"
-    fi
     if (( $(<"$_tmux_update_env_path") > $_tmux_update_env_last_change )); then
       eval "$(tmux show-environment -s)"
       _tmux_update_env_last_change=$(<"$_tmux_update_env_path")
